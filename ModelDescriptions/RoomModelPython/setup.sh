687a3d580d2a5b86edc788f1e33ec06eb206de79
devabz <devabzko@gmail.com>
2025-03-16 13:07:46 +0100

    updated devcontainer.json

--- ModelDescriptions/RoomModelPython/setup.sh
+++ ModelDescriptions/RoomModelPython/setup.sh
@@ -7,9 +7,3 @@ apt install gnuplot -y
 
 # REMOVE EXCESS PYTHON INTERPRETERS 
 rm -rf $(find /usr/bin -type f -name python*)
-
-# INSTALL NECESSARY VSCODE EXTENSIONS
-apt update
-apt install -y code
-code --install-extension ms-python.python
-code --install-extension ms-toolsai.jupyter
