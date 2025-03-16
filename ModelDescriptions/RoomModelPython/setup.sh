3aaa6d2880d70a0686b451e87e6594fabf12a76d
devabz <devabzko@gmail.com>
2025-03-16 12:48:46 +0100

    updated devcontainer.json

--- ModelDescriptions/RoomModelPython/setup.sh
+++ ModelDescriptions/RoomModelPython/setup.sh
@@ -1,4 +1,13 @@
+# INSTALL REQUIREMENTS
 pip install --user -r requirements.txt
+
+# INSTALL GNUPLOT
 apt update 
 apt install gnuplot -y
+
+# REMOVE EXCESS PYTHON INTERPRETERS 
 rm -rf $(find /usr/bin -type f -name python*)
+
+# INSTALL NECESSARY VSCODE EXTENSIONS
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
